da4af037e8e7e8f96aeca33ea5c27cc81d39f9af
33tm <80350771+33tm@users.noreply.github.com>
2024-04-06 18:11:38 -0700

    eslint

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,5 @@
 import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
 
 import { App } from "@/app/app/app"
 import { Toggle } from "@/components/toggle"
